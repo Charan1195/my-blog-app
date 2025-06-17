68ddac6ac281d42dab7ae78333124ec171be514d
Charan1195 <Charan.kg006@gmail.com>
2025-06-17 20:43:58 +0530

    Create charankgApplication.java

---
+++ src/main/java/com/example/charankgApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharanKgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharanKgApplication.class, args);
+    }
+}
