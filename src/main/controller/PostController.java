655fce55e06b1a6d7a74a1bc5791686e61fa7219
Charan1195 <Charan.kg006@gmail.com>
2025-06-17 17:54:28 +0530

    Update PostController.java

--- src/main/controller/PostController.java
+++ src/main/controller/PostController.java
@@ -1,1 +1,28 @@
+package com.example.blog.controller;
+
+import com.example.blog.model.Post;
+import com.example.blog.repository.PostRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private final PostRepository postRepo;
+
+    public PostController(PostRepository postRepo) {
+        this.postRepo = postRepo;
+    }
+
+    @GetMapping
+    public List<Post> getAll() {
+        return postRepo.findAll();
+    }
+
+    @PostMapping
+    public Post create(@RequestBody Post post) {
+        return postRepo.save(post);
+    }
+}
 
