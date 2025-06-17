f7494ae18fcaddcff2851738a9f13b9579cbc73e
Charan1195 <Charan.kg006@gmail.com>
2025-06-17 17:53:11 +0530

    Update Post.java

--- src/main/model/Post.java
+++ src/main/model/Post.java
@@ -1,1 +1,18 @@
+package com.example.blog.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+}
 
