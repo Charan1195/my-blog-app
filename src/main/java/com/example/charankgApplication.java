a5386b2d4d8b9fe0cd44430e59a6695e5347374c
Charan1195 <Charan.kg006@gmail.com>
2025-06-18 09:57:10 +0530

    Update charankgApplication.java

--- src/main/java/com/example/charankgApplication.java
+++ src/main/java/com/example/charankgApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CharanKgApplication {
-
+public class CharankgApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CharanKgApplication.class, args);
+        SpringApplication.run(CharankgApplication.class, args);
     }
 }
